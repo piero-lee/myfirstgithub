47dcdccfe8d7c8a095d6ae891d4131dcb8243445
piero-lee <ljq_p_cn@163.com>
2016-01-15 23:12:57 +0800

    20160115-1
    验证码作成

--- sitoa/Application/Home/Controller/PublicController.class.php
+++ sitoa/Application/Home/Controller/PublicController.class.php
@@ -23,16 +23,30 @@ class PublicController extends Controller {
         }
     }
 
+    // 检测输入的验证码是否正确，$code为用户输入的验证码字符串
+    private function check_verify($code, $id = '') {
+        $verify = new \Think\Verify();
+        return $verify -> check($code, $id);
+    }
+
+    // 登录检测
+    public function check_login() {
+        $check = $this -> check_verify($_POST['verify'],'');
+        if (!$check) {
+            $this -> error('验证码错误！');
+        }
+    }
 
     /**生成验证码*/
-    public function verfiy_c(){
+    public function verify_c(){
         $config = array('fontSize' => 15, // 验证码字体大小
             'length' => 4,                 // 验证码位数
             'useNoise' => false,         // 关闭验证码杂点
             'fontttf' => '5.ttf',        // 选择验证码字体
             'useImgBg' => true,          // 开启背景图片
-            'useZh' => true              // 使用中文
+            //'useZh' => true              // 使用中文
         );
+        ob_clean();
         $verify = new \Think\Verify($config);
         $verify -> entry();
     }
--- sitoa/Runtime/Cache/Home/ddaf032eeb3f0e441c9409c65fc90ee1.php
+++ sitoa/Runtime/Cache/Home/ddaf032eeb3f0e441c9409c65fc90ee1.php
@@ -19,6 +19,7 @@
     <link href="/sitoa/Public/css/essentials.css" rel="stylesheet" type="text/css" />
     <link href="/sitoa/Public/css/layout.css" rel="stylesheet" type="text/css" />
     <link href="/sitoa/Public/css/color_scheme/green.css" rel="stylesheet" type="text/css" id="color_scheme" />
+
     <!--雪花特效 START-->
     <link rel="stylesheet" type="text/css" href="/sitoa/Public/css/snow-normalize.css" />
     <link rel="stylesheet" type="text/css" href="/sitoa/Public/css/snow-default.css">
@@ -26,9 +27,7 @@
     <link rel="stylesheet" type="text/css" href="/sitoa/Public/css/login/style1.css" />
     <script type="text/javascript" src="/sitoa/Public/js/modernizr.custom.86080.js"></script>
     <style type="text/css">
-        .fontstyle {
-            color:#84C1FF;font-weight: bold;
-        }
+        .fontstyle { color:#84C1FF;font-weight: bold;}
         .snow-container{position: fixed; top: 0; left: 0; width: 100%; height: 100%; pointer-events: none; z-index: 100001;}
     </style>
     <!--雪花特效 END-->
@@ -84,6 +83,18 @@
 								<input type="password" style="BACKGROUND-COLOR: transparent;">
 								<b class="tooltip tooltip-top-right">请输入密码</b>
 							</label>
+						</section>
+						<section>
+							<label class="label"><div class="fontstyle">验证码</div></label>
+							<div class="col-sm-9 row" style="margin-left: -30px;">
+								<div class="col-xs-6">
+									<input class="form-control" id="verify" name="verify" style="BACKGROUND-COLOR: transparent;" MAXLENGTH="4" />
+								</div>
+								<div class="col-xs-6">
+									<!--<img src="<?php echo U('Home/Index/verify_c',array());?>" style='cursor:pointer' title='刷新验证码' id='verifyImg' onclick='freshVerify()'>-->
+                                    <img src="<?php echo U('verify_c',array());?>" style='cursor:pointer' title='刷新验证码' id='verifyImg' onclick='freshVerify()'>
+								</div>
+							</div>
                             <label class="switch switch-default switch-round">
                                 <input type="checkbox" value="1">
                                 <span class="switch-label" data-on="YES" data-off="NO"></span>
@@ -92,7 +103,7 @@
 						</section>
 					</fieldset>
 					<footer>
-                        <a href="#" class="btn btn-sm btn-default btn-bordered pull-right">
+                        <a href="#" class="btn btn-sm btn-default btn-bordered pull-right" onclick="login();">
                             <span>&nbsp;&nbsp;登录&nbsp;&nbsp;</span>
                         </a>
 						<div class="forgot-password pull-left">
@@ -106,6 +117,10 @@
     </div>
 	
 
+<!-- Custom and plugin javascript -->
+<script src="/sitoa/Public/js/common.js"></script>
+
+
 	<!-- JAVASCRIPT FILES -->
 	<script type="text/javascript">var plugin_path = '/sitoa/Public/plugins/';</script>
 	<script type="text/javascript" src="/sitoa/Public/plugins/jquery/jquery-2.1.4.min.js"></script>
@@ -115,6 +130,21 @@
 	<script type="text/javascript" src="/sitoa/Public/js/snow-jquery-2.1.1.min.js"></script>
 	<script type="text/javascript" src="/sitoa/Public/js/snow-three.js"></script>
 	<script type="text/javascript">
+        function login() {
+            sendForm("form_login", "<?php echo U('public/check_login');?>");
+        }
+
+        $(document).ready(function() {
+            var $dom = "#password";
+            <?php if(!empty($is_verify_code)): ?>$dom="#verify";<?php endif; ?>
+            $($dom).keydown(function(event) {
+                if (event.keyCode == 13) {
+                    sendForm("form_login", "<?php echo U('public/check_login');?>");
+                    return false;
+
+                }
+            });
+        });
 		function randomRange(t,i){return Math.random()*(i-t)+t}Particle3D=function(t){THREE.Particle.call(this,t),this.velocity=new THREE.Vector3(0,-2,0),this.velocity.rotateX(randomRange(-45,45)),this.velocity.rotateY(randomRange(0,360)),this.gravity=new THREE.Vector3(0,0,0),this.drag=1},Particle3D.prototype=new THREE.Particle,Particle3D.prototype.constructor=Particle3D,Particle3D.prototype.updatePhysics=function(){this.velocity.multiplyScalar(this.drag),this.velocity.addSelf(this.gravity),this.position.addSelf(this.velocity)};var TO_RADIANS=Math.PI/180;THREE.Vector3.prototype.rotateY=function(t){cosRY=Math.cos(t*TO_RADIANS),sinRY=Math.sin(t*TO_RADIANS);var i=this.z,o=this.x;this.x=o*cosRY+i*sinRY,this.z=o*-sinRY+i*cosRY},THREE.Vector3.prototype.rotateX=function(t){cosRY=Math.cos(t*TO_RADIANS),sinRY=Math.sin(t*TO_RADIANS);var i=this.z,o=this.y;this.y=o*cosRY+i*sinRY,this.z=o*-sinRY+i*cosRY},THREE.Vector3.prototype.rotateZ=function(t){cosRY=Math.cos(t*TO_RADIANS),sinRY=Math.sin(t*TO_RADIANS);var i=this.x,o=this.y;this.y=o*cosRY+i*sinRY,this.x=o*-sinRY+i*cosRY};
 		$(function(){var container=document.querySelector(".snow-container");if(/MSIE 6|MSIE 7|MSIE 8/.test(navigator.userAgent)){return}else{if(/MSIE 9|MSIE 10/.test(navigator.userAgent)){$(container).css("height",$(window).height()).bind("click",function(){$(this).fadeOut(1000, function(){$(this).remove()})})}}var containerWidth=$(container).width();var containerHeight=$(container).height();var particle;var camera;var scene;var renderer;var mouseX=0;var mouseY=0;var windowHalfX=window.innerWidth/2;var windowHalfY=window.innerHeight/2;var particles=[];var particleImage=new Image();particleImage.src="/sitoa/Public/img/snow.png";var snowNum=500;function init(){camera=new THREE.PerspectiveCamera(75,containerWidth/containerHeight,1,10000);camera.position.z=1000;scene=new THREE.Scene();scene.add(camera);renderer=new THREE.CanvasRenderer();renderer.setSize(containerWidth,containerHeight);var material=new THREE.ParticleBasicMaterial({map:new THREE.Texture(particleImage)});for(var i=0;i<snowNum;i++){particle=new Particle3D(material);particle.position.x=Math.random()*2000-1000;particle.position.y=Math.random()*2000-1000;particle.position.z=Math.random()*2000-1000;particle.scale.x=particle.scale.y=1;scene.add(particle);particles.push(particle)}container.appendChild(renderer.domElement);document.addEventListener("mousemove",onDocumentMouseMove,false);document.addEventListener("touchstart",onDocumentTouchStart,false);document.addEventListener("touchmove",onDocumentTouchMove,false);setInterval(loop,1000/40)}function onDocumentMouseMove(event){mouseX=event.clientX-windowHalfX;mouseY=event.clientY-windowHalfY}function onDocumentTouchStart(event){if(event.touches.length==1){event.preventDefault();mouseX=event.touches[0].pageX-windowHalfX;mouseY=event.touches[0].pageY-windowHalfY}}function onDocumentTouchMove(event){if(event.touches.length==1){event.preventDefault();mouseX=event.touches[0].pageX-windowHalfX;mouseY=event.touches[0].pageY-windowHalfY}}function loop(){for(var i=0;i<particles.length;i++){var particle=particles[i];particle.updatePhysics();with(particle.position){if(y<-1000){y+=2000}if(x>1000){x-=2000}else{if(x<-1000){x+=2000}}if(z>1000){z-=2000}else{if(z<-1000){z+=2000}}}}camera.position.x+=(mouseX-camera.position.x)*0.005;camera.position.y+=(-mouseY-camera.position.y)*0.005;camera.lookAt(scene.position);renderer.render(scene,camera)}init()});
 	</script>
